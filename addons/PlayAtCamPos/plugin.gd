6ac159cf0c74ea7b587e4c41a7d428e1bb0938c3
AdilDevStuff <adilshafiq784@gmail.com>
2024-02-11 21:41:55 +0500

    Added error message when no target in scene

--- addons/PlayAtCamPos/plugin.gd
+++ addons/PlayAtCamPos/plugin.gd
@@ -2,20 +2,25 @@
 extends EditorPlugin
 
 var cam
+var target
 var play_here_btn
-var player
+var warning_dialogue
 
 var follow_rotation: bool = false
 var target_group: String = ""
+
 const PLAY_HERE_BUTTON = preload("res://addons/PlayAtCamPos/play_here_button.tscn")
+const WARNING_DIALOGUE = preload("res://addons/PlayAtCamPos/warning_dialogue.tscn")
 
 func _enter_tree():
 	set_project_setting()
+
 	cam = get_editor_interface().get_editor_viewport_3d().get_camera_3d()
 	
+	# Instance the play button
 	play_here_btn = PLAY_HERE_BUTTON.instantiate()
 	play_here_btn.pressed.connect(_on_button_pressed)
-
+	
 	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, play_here_btn)
 
 func _process(delta):
@@ -27,9 +32,13 @@ func _exit_tree():
 	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, play_here_btn)
 
 func get_target():
-	for child in get_editor_interface().get_edited_scene_root().get_children():
+	for child in get_tree().edited_scene_root.get_children():
 		if child is CharacterBody3D:
-			player = child
+			target = child
+		else:
+			target = null
+	if target == null:
+		printerr("Target not found! Make sure you have a CharacterBody3D in the current scene")
 
 func get_camera_position():
 	return cam.position
@@ -54,10 +63,11 @@ func get_project_setting():
 
 func _on_button_pressed():
 	get_target()
-	match follow_rotation:
-		true:
-			player.transform = get_camera_transform()
-		false:
-			player.position = get_camera_position()
-			player.rotation = Vector3.ZERO
-	get_editor_interface().play_current_scene()
+	if target != null:
+		match follow_rotation:
+			true:
+				target.transform = get_camera_transform()
+			false:
+				target.position = get_camera_position()
+				target.rotation = Vector3.ZERO
+		get_editor_interface().play_current_scene()
